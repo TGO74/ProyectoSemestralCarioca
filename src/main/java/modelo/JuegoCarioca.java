e35ffebfe1247a5c9266b01dd181e283629db6b9
DiegoGM98 <132071916+DiegoGM98@users.noreply.github.com>
2023-12-08 15:34:46 -0300

    Update JuegoCarioca.java

--- src/main/java/modelo/JuegoCarioca.java
+++ src/main/java/modelo/JuegoCarioca.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class JuegoCarioca {
 
     ArrayList<Carta> cartas = new ArrayList<Carta>();
+    ArrayList<Jugador> jugadores = new ArrayList<Jugador>();  // Agregamos una lista para los jugadores
 
     Baraja baraja = new Baraja();
 
@@ -14,22 +15,41 @@ public class JuegoCarioca {
         cartas = baraja.getCartasBaraja();
     }
 
-    public void iniciarJuego(ArrayList<Jugador> jugadores) {
+    // Método para agregar jugadores al juego
+    public void agregarJugador(Jugador jugador) {
+        jugadores.add(jugador);
+    }
+
+    // Método para obtener los jugadores del juego
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void iniciarJuego() {  // Modificamos este método para usar la lista de jugadores interna
         baraja.repartirCartas(jugadores);
     }
 
-    public void realizarRondas(ArrayList<Jugador> jugadores) {
+    public void realizarRondas() {  // Modificamos este método para usar la lista de jugadores interna
         for (Jugador jugador : jugadores) {
             jugador.jugar();
         }
     }
 
-
-    public void finalizarJuego(ArrayList<Jugador> jugadores) {
+    public void finalizarJuego() {  // Modificamos este método para usar la lista de jugadores interna
         for (Jugador jugador : jugadores) {
             if (jugador.getMano().size() == 0) {
                 System.out.println("El ganador es: " + jugador.getNombre());
             }
         }
+    }
+
+    // Método para obtener el ganador del juego
+    public Jugador getGanador() {
+        for (Jugador jugador : jugadores) {
+            if (jugador.getMano().size() == 0) {
+                return jugador;
+            }
+        }
+        return null;
     }
 }
